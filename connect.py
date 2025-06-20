45ad23a84d421913c710cd7b34aa05fc4f8a4006
gacon2004 <quangminhshop6789@gmail.com>
2025-06-21 02:30:30 +0700

    init fastapi project

--- connect.py
+++ connect.py
@@ -8,7 +8,9 @@ def get_connection():
     conn = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
+        port=int(os.getenv("DB_PORT")),  # Thêm dòng này
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
     )
     return conn
+# mysql://root:kjsdziATJUhZtuGpJlYCInSGmkCmJvks@switchback.proxy.rlwy.net:10072/railway
